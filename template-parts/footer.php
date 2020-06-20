446bb8ba99972ab4ea000b54199896b305362e2e
James Hill <jhill7177@gmail.com>
2020-06-21 03:22:39 +1000

    - added page width to header and footer - added styles to style.css

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,20 +1,22 @@
       <footer>
-        <nav id="footer-menu">
-          <ul>
-            <li><a class="footer-menu-item sans-serif" href="/about-me.html">About Me</a></li>
-            <li><a class="footer-menu-item sans-serif" href="/resume.html">Resume</a></li>
-            <li><a class="footer-menu-item sans-serif" href="/projects.html">Projects</a></li>
-            <li><a class="footer-menu-item sans-serif" href="/projects.html">Dev-Notes</a></li>
-            <li><a class="footer-menu-item sans-serif" href="/contact.html">Contact</a></li>
-          </ul>
-        </nav>
-        <nav id="social-menu">
-          <ul>
-            <li><a class="social-menu-item" href="https://www.linkedin.com/in/james-hill-7636181a6/"><i class="fab fa-linkedin"></i></a></li>
-            <li><a class="social-menu-item" href="https://github.com/JamesAlexanderHill"><i class="fab fa-github-square"></i></a></li>
-            <li><a class="social-menu-item" href="mailto:info@jamesalexanderhill.com"><i class="fas fa-envelope-square"></i></a></li>
-          </ul>
-        </nav>
+        <div class="page-width">
+          <nav id="footer-menu">
+            <ul>
+              <li><a class="footer-menu-item sans-serif" href="/about-me.html">About Me</a></li>
+              <li><a class="footer-menu-item sans-serif" href="/resume.html">Resume</a></li>
+              <li><a class="footer-menu-item sans-serif" href="/projects.html">Projects</a></li>
+              <li><a class="footer-menu-item sans-serif" href="/projects.html">Dev-Notes</a></li>
+              <li><a class="footer-menu-item sans-serif" href="/contact.html">Contact</a></li>
+            </ul>
+          </nav>
+          <nav id="social-menu">
+            <ul>
+              <li><a class="social-menu-item" href="https://www.linkedin.com/in/james-hill-7636181a6/"><i class="fab fa-linkedin"></i></a></li>
+              <li><a class="social-menu-item" href="https://github.com/JamesAlexanderHill"><i class="fab fa-github-square"></i></a></li>
+              <li><a class="social-menu-item" href="mailto:info@jamesalexanderhill.com"><i class="fas fa-envelope-square"></i></a></li>
+            </ul>
+          </nav>
+        </div>
       </footer>
     </main>
   <?php wp_footer(); ?>
--- template-parts/header.php
+++ template-parts/header.php
@@ -10,15 +10,17 @@
     <?php get_template_part('template-parts/sidebar'); ?>
     <main id="main">
       <header>
-        <a id="sidebar-menu-btn" class="btn" onclick="toggleSidebar()"><i class="fas fa-bars"></i></a>
-        <a class="title" href="/index.html"><h1 class="sans-serif">James Hill</h1></a>
-        <nav id="main-menu">
-          <ul>
-            <li><a class="main-menu-item" href="/about-me.html">About Me</a></li>
-            <li><a class="main-menu-item" href="/resume.html">Resume</a></li>
-            <li><a class="main-menu-item" href="/projects.html">Projects</a></li>
-            <li><a class="main-menu-item" href="/projects.html">Dev-Notes</a></li>
-            <li><a class="main-menu-item" href="/contact.html">Contact</a></li>
-          </ul>
-        </nav>
+        <div class="page-width">
+          <a id="sidebar-menu-btn" class="btn" onclick="toggleSidebar()"><i class="fas fa-bars"></i></a>
+          <h1 class="title sans-serif"><a href="/index.html">James Hill</a></h1>
+          <nav id="main-menu">
+            <ul>
+              <li><a class="main-menu-item" href="/about-me.html">About Me</a></li>
+              <li><a class="main-menu-item" href="/resume.html">Resume</a></li>
+              <li><a class="main-menu-item" href="/projects.html">Projects</a></li>
+              <li><a class="main-menu-item" href="/projects.html">Dev-Notes</a></li>
+              <li><a class="main-menu-item" href="/contact.html">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
